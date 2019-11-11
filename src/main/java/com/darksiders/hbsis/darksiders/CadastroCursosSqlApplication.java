32a46e354c2068a29fd06f76fd69593f2cfa561d
AllanYuriBM <allan.metzner@hbsis.com.br>
2019-11-11 10:33:11 -0300

    first commit

---
+++ src/main/java/com/darksiders/hbsis/darksiders/CadastroCursosSqlApplication.java
@@ -0,0 +1,13 @@
+package com.darksiders.hbsis.darksiders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroCursosSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroCursosSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darksiders/hbsis/darksiders/CadastroCursosSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darksiders.hbsis.darksiders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroCursosSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
